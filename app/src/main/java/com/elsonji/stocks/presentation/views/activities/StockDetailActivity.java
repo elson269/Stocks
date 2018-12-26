908bcb37d8ee54560862f1843e1f6c8c89c3fbf9
Elson <elson.ji@gmail.com>
2018-12-25 23:59:57 -0500

    feat: Build views.

---
+++ app/src/main/java/com/elsonji/stocks/presentation/views/activities/StockDetailActivity.java
@@ -0,0 +1,15 @@
+package com.elsonji.stocks.presentation.views.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.elsonji.stocks.R;
+
+public class StockDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock_detail);
+    }
+}
